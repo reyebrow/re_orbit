ebf27e0c91f3899d526588451d6c17c3a1d29bbe
Christopher Torgalson <chris@raisedeyebrow.com>
2012-01-17 11:34:18 -0800

    Add css; coder module changes; make re_orbit.js match distro theme

--- js/re_orbit.js
+++ js/re_orbit.js
@@ -9,9 +9,9 @@
  * @see http://www.zurb.com/playground/orbit-jquery-image-slider
  */
 (function ($) {
-  Drupal.behaviors.re_orbit = {
+  Drupal.behaviors.orbit = {
     attach: function(context) {
-      $('.view-id-slideshow.view-display-id-block .view-content').orbit({
+      $('#featured').orbit({
         animation: 'fade', // fade, horizontal-slide, vertical-slide, horizontal-push
         animationSpeed: 800, // how fast animtions are
         timer: true, // true or false to have the timer
@@ -29,6 +29,25 @@
         afterSlideChange: function(){}, // empty function
         fluid: true // or set a aspect ratio for content slides (ex: '4x3')
       });
+
+      $('#photo_gallery').orbit({
+        animation: 'horizontal-slide', // fade, horizontal-slide, vertical-slide, horizontal-push
+        animationSpeed: 800, // how fast animtions are
+        timer: true, // true or false to have the timer
+        advanceSpeed: 4000, // if timer is enabled, time between transitions
+        pauseOnHover: false, // if you hover pauses the slider
+        startClockOnMouseOut: false, // if clock should start on MouseOut
+        startClockOnMouseOutAfter: 1000, // how long after MouseOut should the timer start again
+        directionalNav: true, // manual advancing directional navs
+        captions: true, // do you want captions?
+        captionAnimation: 'fade', // fade, slideOpen, none
+        captionAnimationSpeed: 800, // if so how quickly should they animate in
+        bullets: true, // true or false to activate the bullet navigation
+        bulletThumbs: false, // thumbnails for the bullets
+        bulletThumbLocation: '', // location from this file where thumbs will be
+        afterSlideChange: function(){}, // empty function
+        fluid: '4x3' // or set a aspect ratio for content slides (ex: '4x3')
+      });      
     }
-  }; /* Drupal.behaviors.re_orbit */
+  }; /* Drupal.behaviors.orbit */
 })(jQuery);
